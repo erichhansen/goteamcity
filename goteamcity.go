b62c2b8ccef5b20cf71adeb71a898b0ac78491bc
Erich Hansen <et_hansen@yahoo.com>
2015-03-28 13:49:21 -0400

    adds credientals for teamcity and json header

--- goteamcity.go
+++ goteamcity.go
@@ -5,24 +5,44 @@ import(
     "encoding/json"
     "os"
     "net/http"
+    "io/ioutil"
+    "log"
 )
+
+const requestPath string = "/httpAuth/app/rest/cctray/projects.xml"
 
 type configuration struct {
     TeamCityUrl string
+    TeamCityUsername string
+    TeamCityPassword string
 }
 
 func GetTeamCityStatus() string {
-    url := getTeamCityUrl()
-    resp, err := http.Get(url)
+    config := getTeamCityConfig()
+    url := config.TeamCityUrl + requestPath;
+
+    client := &http.Client{}
+
+    req, err := http.NewRequest("GET", url, nil)
+    req.Header.Add("Accept", "application/json")
+    req.SetBasicAuth(config.TeamCityUsername, config.TeamCityPassword)
+
+    resp, err := client.Do(req)
     if err != nil {
-        fmt.Println("error:", err)
+        log.Fatalf("Error: %s", err)
+    } else {
+        defer resp.Body.Close()
+        contents, err := ioutil.ReadAll(resp.Body)
+        if err != nil {
+            log.Fatalf("Error: %s", err)
+        }
+        fmt.Println(string(contents))
     }
 
-    fmt.Println(resp)
-    return "Failed"
+    return "ToDo"
 }
   
-func getTeamCityUrl() string {
+func getTeamCityConfig() configuration {
     file, err := os.Open("conf.json")
     if err != nil {
         fmt.Println("error:", err)
@@ -33,6 +53,5 @@ func getTeamCityUrl() string {
     if err != nil {
         fmt.Println("error:", err)
     }
-    fmt.Println(config.TeamCityUrl)
-    return config.TeamCityUrl;
+    return config;
 }
