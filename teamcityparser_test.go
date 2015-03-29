701b93b9711eb5ac0214ee9aa0ae51eccafc3e9c
Erich Hansen <et_hansen@yahoo.com>
2015-03-29 15:39:45 -0400

    add teamcity reader tests

--- teamcityparser_test.go
+++ teamcityparser_test.go
@@ -19,7 +19,6 @@ func (s stubInvestigationReader) ReadInvestigation(resp io.ReadCloser) bool {
     return s.Investigating
 }
 
-
 func TestParseResponseShouldBeFail(t *testing.T) {
     file, err := os.Open("testfiles/teamcity1.json")
     if err != nil {
@@ -31,5 +30,42 @@ func TestParseResponseShouldBeFail(t *testing.T) {
     
     if status != Fail {
     	t.Fatalf("Should be Fail state")
+    }
+}
+
+func TestParseResponseShouldBeInvestigating(t *testing.T) {
+    file, err := os.Open("testfiles/teamcity1.json")
+    if err != nil {
+        fmt.Println("error:", err)
+    }
+
+    stub := stubInvestigationReader{Investigating: true}
+    status := parseResponse(file, stub)
+    
+    if status != Investigating {
+        t.Fatalf("Should be Investigating state")
+    }
+}
+
+func TestParseResponseShouldBeSucess(t *testing.T) {
+    file, err := os.Open("testfiles/teamcity2.json")
+    if err != nil {
+        fmt.Println("error:", err)
+    }
+
+    stub := stubInvestigationReader{Investigating: true}
+    status := parseResponse(file, stub)
+    
+    if status != Success {
+        t.Fatalf("Should be Success state")
+    }
+}
+
+func TestParseNameShouldParse(t *testing.T) {
+    projectName := "Example :: ProjectName"
+    actualName := parseName(projectName)
+
+    if actualName != "ProjectName" {
+        t.Fatalf("Name did not parse correctly. Expected ProjectName got %s", actualName)
     }
 }
